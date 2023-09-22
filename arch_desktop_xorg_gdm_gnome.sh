74c85878252b4ed59f72aca455e1ab2099574b16
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-09-22 15:44:14 +0200

    Update arch_desktop_xorg_gdm_gnome.sh

--- arch_desktop_xorg_gdm_gnome.sh
+++ arch_desktop_xorg_gdm_gnome.sh
@@ -2,22 +2,14 @@
 sudo pacman --disable-download-timeout -Syu
 
 # Install Xorg
-sudo pacman --disable-download-timeout -Sy xorg xorg-server
+sudo pacman --disable-download-timeout --noconfirm -Sy xorg xorg-server
 
 # Install Gnome & GDM
-sudo pacman --disable-download-timeout -noconfirm -Sy gdm gnome-session gnome-shell gnome-shell-extensions gnome-tweaks
+sudo pacman --disable-download-timeout --noconfirm -Sy gdm gnome-session gnome-shell gnome-shell-extensions gnome-tweaks
 
 # Install Gnome core applications
-sudo pacman --disable-download-timeout -noconfirm -Sy baobab eog evince gnome-background gnome-calculator gnome-clocks gnome-console gnome-control-center gnome-disk-utility gnome-keyring gnome-logs gnome-menus gnome-photos gnome-settings-daemon gnome-system-monitor gnome-text-editor nautilus
+sudo pacman --disable-download-timeout --noconfirm -Sy baobab eog evince gnome-background gnome-calculator gnome-clocks gnome-console gnome-control-center gnome-disk-utility gnome-keyring gnome-logs gnome-menus gnome-photos gnome-settings-daemon gnome-system-monitor gnome-text-editor nautilus
 #gnome-color-manager sushi
-
-# Remove unnecessary packages
-#gnome-remote-desktop gnome-software gnome-user-share gnome-video-effects
-
-#TOOLS="gnome-calendar gnome-contacts gnome-maps gnome-music gnome-remote-desktop gnome-tour gnome-user-docs gnome-user-share gnome-weather"
-#for Tool in $TOOLS; do
-    #pacman -Rs --noconfirm $Tool
-#done
 
 # Ensure keyboard layout is German
 sudo localectl set-keymap de

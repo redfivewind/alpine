664463703f57c696cc71a9789c5cb623b1ffa5e2
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-09-23 15:34:50 +0200

    Update arch_vbox.sh

--- arch_vbox.sh
+++ arch_vbox.sh
@@ -1,8 +1,8 @@
 # System update
-pacman --disable-download-timeout --noconfirm -Syu
+sudo pacman --disable-download-timeout --noconfirm -Syu
 
 # Install VirtualBox guest addition
-pacman --disable-download-timeout --noconfirm -Sy virtualbox-guest-utils
+sudo pacman --disable-download-timeout --noconfirm -Sy virtualbox-guest-utils
 
 # Remove packages that are no longer required
 sudo pacman --noconfirm -Rns $(pacman -Qdtq)

67a33a9b6b30872122d2baf78666e9911f417b8a
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-09-23 15:31:40 +0200

    Update arch_aur_yay.sh

--- arch_aur_yay.sh
+++ arch_aur_yay.sh
@@ -2,17 +2,16 @@
 mkdir ~/tools
 cd ~/tools
 git clone https://aur.archlinux.org/yay.git
+cd yay
 makepkg -si
 yay --version
 
-
 # Install required tools
-sudo yay -Syu
+sudo yay --disable-download-timeout --noconfirm -Syu
 TOOLS="chkrootkit secure-delete"
 for Tool in $TOOLS; do
-    yay -Sy --noconfirm $Tool
+    yay --disable-download-timeout --noconfirm -Sy $Tool
 done
-
 
 # Remove packages that are no longer required
 sudo pacman -Rns $(pacman -Qdtq)

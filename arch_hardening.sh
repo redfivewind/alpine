51dc3359805012b731aa2f917031443a9c08e76f
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-09-23 15:39:50 +0200

    Update arch_hardening.sh

--- arch_hardening.sh
+++ arch_hardening.sh
@@ -29,7 +29,7 @@ echo "unset HISTFILE" >> ~/.bash_profile
 
 
 # New MAC address at each reboot
-sudo pacman -Sy --noconfirm macchanger
+sudo pacman --disable-download-timeout --noconfirm -Sy macchanger
 
 
 # New hostname at each reboot

dd77c23a5eeafb48340a0cce4e0500a8d8f9de00
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-09-23 14:40:50 +0200

    Update arch_xorg_lightdm_xfce.sh

--- arch_xorg_lightdm_xfce.sh
+++ arch_xorg_lightdm_xfce.sh
@@ -12,7 +12,7 @@ sudo pacman --disable-download-timeout --noconfirm -S xorg xorg-drivers
 sudo pacman --disable-download-timeout --noconfirm -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4
 
 # Install additional applications
-sudo pacman --disable-download-timeout --noconfirm -S mousepad ristretto thunar-archive-plugin thunar-media-tags-plugin xfce4-artwork xfce4-cpugraph-plugin xfce4-mount-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-taskmanager
+sudo pacman --disable-download-timeout --noconfirm -S alsa-plugins alsa-utils mousepad ristretto thunar-archive-plugin thunar-media-tags-plugin xfce4-artwork xfce4-cpugraph-plugin xfce4-mount-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-taskmanager
 
 # Remove unnecessary applications
 #

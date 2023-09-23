4458b3f79e2c7047bcf07fc7fdd992825a9ef843
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-09-23 11:29:38 +0200

    Update arch_xorg_lightdm_xfce.sh

--- arch_xorg_lightdm_xfce.sh
+++ arch_xorg_lightdm_xfce.sh
@@ -1,15 +1,21 @@
 # System update
 sudo pacman --disable-download-timeout --noconfirm -Syu
 
-# Install Xorg, LightDM and Xfce
-sudo pacman --disable-download-timeout --noconfirm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 xorg xorg-server
+# Install Xorg
+sudo pacman --disable-download-timeout --noconfirm xorg xorg-drivers
+
+# Install LightDM and Xfce
+sudo pacman --disable-download-timeout --noconfirm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4
 
 # Install additional applications
+#
 
+# Remove unnecessary applications
+#
 
 # Configure LightDM service
-sudo systemctl enable lightdm
-sudo systemctl start lightdm
+sudo systemctl enable lightdm.service
+sudo systemctl start lightdm.service
 
 # Remove packages that are no longer required
 sudo pacman -Rns $(pacman -Qdtq)

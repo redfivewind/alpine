817a6e1b6ed806f5d41f294063d72053714082b3
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-09-25 09:36:19 +0200

    Update arch_audio.sh

--- arch_audio.sh
+++ arch_audio.sh
@@ -2,7 +2,7 @@
 pacman --disable-download-timeout --noconfirm -Syu
 
 # Install ALSA and PulseAudio
-pacman --disable-download-timeout --noconfirm -Sy alsa-plugins alsa-utils pulseaudio pulseaudio-plugins pulseaudio-utils
+pacman --disable-download-timeout --needed --noconfirm -Sy alsa-plugins alsa-utils pulseaudio pulseaudio-plugins pulseaudio-utils
 
 # Remove packages that are no longer required
 sudo pacman --noconfirm -Rns $(pacman -Qdtq)

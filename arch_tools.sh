cd37942e17c4e03b6e6cac97206b17bb40127c9f
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-09-25 10:06:14 +0200

    Update arch_tools.sh

--- arch_tools.sh
+++ arch_tools.sh
@@ -2,7 +2,6 @@
 echo "Updating the system..."
 pacman --disable-download-timeout --needed --noconfirm -Syu
 
-
 # Install yay for access to the AUR ecosystem
 mkdir ~/tools
 cd ~/tools
@@ -10,16 +9,18 @@ git clone https://aur.archlinux.org/yay.git
 makepkg -si
 yay --version
 
-
 # Pacman software
 TOOLS="dmidecode git gparted rkhunter wget"
 for Tool in $TOOLS; do
     sudo pacman --disable-download-timeout --needed --noconfirm -S $Tool
 done
 
-
 # AUR software
 TOOLS="chkrootkit secure-delete"
 for Tool in $TOOLS; do
     sudo yay --disable-download-timeout --needed --noconfirm -S $Tool
 done
+
+# Clean up
+sudo pacman -Rns $(pacman -Qdtq)
+sudo paccache -r

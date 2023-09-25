1d0d6cd067b45def963a51a136d6ceb30396ab08
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-09-25 09:37:18 +0200

    Update arch_xorg_lightdm_xfce.sh

--- arch_xorg_lightdm_xfce.sh
+++ arch_xorg_lightdm_xfce.sh
@@ -4,16 +4,16 @@ sudo localectl set-keymap de
 
 # System update
 echo "Updating the system..."
-pacman --disable-download-timeout --noconfirm -Syu
+pacman --disable-download-timeout --needed --noconfirm -Syu
 
 # Install Xorg
-sudo pacman --disable-download-timeout --noconfirm -S xorg xorg-drivers
+sudo pacman --disable-download-timeout --needed --noconfirm -S xorg xorg-drivers
 
 # Install LightDM and Xfce
-sudo pacman --disable-download-timeout --noconfirm -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4
+sudo pacman --disable-download-timeout --needed --noconfirm -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4
 
 # Install additional applications
-sudo pacman --disable-download-timeout --noconfirm -S mousepad ristretto thunar-archive-plugin thunar-media-tags-plugin xarchiver xfce4-artwork xfce4-cpugraph-plugin xfce4-mount-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-taskmanager
+sudo pacman --disable-download-timeout --needed --noconfirm -S mousepad ristretto thunar-archive-plugin thunar-media-tags-plugin xarchiver xfce4-artwork xfce4-cpugraph-plugin xfce4-mount-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-taskmanager
 
 # Configure LightDM service
 sudo systemctl enable lightdm.service

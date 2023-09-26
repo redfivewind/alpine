4c05f0757dcf45b874b4a95affdd73ca86fc4e19
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-09-26 11:56:19 +0200

    Update arch_vm_general.sh

--- arch_vm_general.sh
+++ arch_vm_general.sh
@@ -1,6 +1,17 @@
 # System update
 sudo pacman --disable-download-timeout --needed --noconfirm -Syu
 
+# Install dependencies
+sudo pacman --disable-download-timeout --needed --noconfirm -S git go
+
+# Install yay for access to the AUR ecosystem
+mkdir ~/tools
+cd ~/tools
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay --version
+
 # Install required tools
 yay --disable-download-timeout --needed --noconfirm -Syu
 TOOLS="chkrootkit chromium gimp keepass libreoffice librewolf obsidian onboard secure-delete thunderbird vlc vscodium xmind"

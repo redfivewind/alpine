f75e720d51d18264eda9fe6b9f99e5776e491246
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-09-26 12:18:23 +0200

    Delete arch_vbox.sh

--- arch_vbox.sh
+++
@@ -1,8 +0,0 @@
-# System update
-sudo pacman --disable-download-timeout --noconfirm -Syu
-
-# Install VirtualBox guest addition
-sudo pacman --disable-download-timeout --needed --noconfirm -S virtualbox-guest-utils
-
-# Clean up
-sudo pacman -Rns $(pacman -Qdtq)

42dde37575f2f55c7e37ba5ee9953835abb4a499
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-09-26 12:03:07 +0200

    Update arch_vm_dev.sh

--- arch_vm_dev.sh
+++ arch_vm_dev.sh
@@ -1,1 +1,26 @@
+# System update
+sudo pacman --disable-download-timeout --needed --noconfirm -Syu
 
+# Install audio capabilities
+sudo pacman --disable-download-timeout --needed --noconfirm -S alsa-plugins alsa-utils pulseaudio
+
+# Install yay for access to the AUR ecosystem
+sudo pacman --disable-download-timeout --needed --noconfirm -S git go
+
+mkdir ~/tools
+cd ~/tools
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay --version
+
+# Install required tools
+yay --disable-download-timeout --needed --noconfirm -Syu
+TOOLS="chkrootkit chromium gimp keepass libreoffice librewolf obsidian onboard pinta secure-delete vlc vscodium xmind"
+for Tool in $TOOLS; do
+    yay --disable-download-timeout --needed --noconfirm -S $Tool
+done
+#dconf-editor
+
+# Clean up
+sudo pacman -Rns $(pacman -Qdtq)

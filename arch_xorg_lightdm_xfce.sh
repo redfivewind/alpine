2c8064ad475f3fa179f0981e29966cb220efa72a
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-09-26 11:44:41 +0200

    Update arch_xorg_lightdm_xfce.sh

--- arch_xorg_lightdm_xfce.sh
+++ arch_xorg_lightdm_xfce.sh
@@ -13,6 +13,10 @@ sudo pacman --disable-download-timeout --needed --noconfirm -S xorg xorg-drivers
 # Install LightDM and Xfce
 sudo pacman --disable-download-timeout --needed --noconfirm -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4
 
+# Set dark theme
+xfconf-query -c xsettings -p /Net/ThemeName -s "Adwaita-dark"
+xfce4-settings-manager --reload
+
 # Install additional applications
 sudo pacman --disable-download-timeout --needed --noconfirm -S alsa-plugins alsa-utils mousepad pulseaudio ristretto thunar-archive-plugin thunar-media-tags-plugin xarchiver xfce4-artwork xfce4-cpugraph-plugin xfce4-mount-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-taskmanager
 

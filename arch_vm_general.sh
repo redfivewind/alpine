e218075ea2b11ff2a3621ed4464f5ddc8efba3da
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-09-26 12:00:54 +0200

    Update arch_vm_general.sh

--- arch_vm_general.sh
+++ arch_vm_general.sh
@@ -16,11 +16,11 @@ yay --version
 
 # Install required tools
 yay --disable-download-timeout --needed --noconfirm -Syu
-TOOLS="chkrootkit chromium gimp keepass libreoffice librewolf obsidian onboard secure-delete thunderbird vlc vscodium xmind"
+TOOLS="chkrootkit chromium gimp keepass libreoffice librewolf obsidian onboard pinta secure-delete thunderbird vlc vscodium xmind"
 for Tool in $TOOLS; do
     yay --disable-download-timeout --needed --noconfirm -S $Tool
 done
-#cheese dconf-editor gimp okular pinta simple-scan
+#cheese dconf-editor gimp okular simple-scan
 
 # Clean up
 sudo pacman -Rns $(pacman -Qdtq)

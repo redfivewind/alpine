aa82a0003680add168c28c204e6b3087bc4b99cf
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-11-10 11:23:37 +0100

    Update arch_vbox.sh

--- arch_vbox.sh
+++ arch_vbox.sh
@@ -1,5 +1,5 @@
 # Install the VirtualBox guest additions
-sudo pacman --disable-download-timeout --needed --noconfirm virtualbox-guest-utils
+sudo pacman -Sy --disable-download-timeout --needed --noconfirm virtualbox-guest-utils
 
 # Enable & start the VirtualBox service
 sudo systemctl enable vboxservice.service

0a3f4d4e45f5a0b7a58d8cdfa300e16d6832e011
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-11-23 14:01:49 +0100

    Create arch_xen.sh

---
+++ arch_xen.sh
@@ -0,0 +1,22 @@
+# Install Xen packages
+yay -S xen xen-qemu
+#sudo pacman -S edk2-ovmf # Fur UEFI support in virtual machines
+#sudo pacman -S seabios # For BIOS support in virtual machines
+
+#FIXME: Build Xen
+
+#FIXME: Modificate the bootloader
+
+#FIXME: Adjust UEFI settings
+
+#FIXME: Install/configure GRUB for XEN
+
+#FIXME: Create a network brigde between dom0 and domU*
+
+#FIXME: Installation of Xen systemd services???
+
+#FIXME: Confirming succesful installation
+
+#FIXME: Configuring best practices???
+
+#FIXME: Xen Hardening

65ea5517573580811cfb4fa5ef140f1073b0d317
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-11-29 12:12:51 +0100

    Update arch_xen.sh

--- arch_xen.sh
+++ arch_xen.sh
@@ -1,3 +1,5 @@
+#FIXME: REVIEW ANYTHING!
+
 # Install Xen packages
 yay -S xen xen-qemu
 #sudo pacman -S edk2-ovmf # Fur UEFI support in virtual machines
@@ -11,8 +13,12 @@ echo "default=xen" >> XEN_CFG_FILE
 echo >> XEN_CFG_FILE
 echo "[xen]" >> XEN_CFG_FILE
 echo "options=console=vga iommu=force:true,qinval:true,debug:true loglvl=all noreboot=true reboot=no vga=ask ucode=scan" >> XEN_CFG_FILE
-echo "kernel=vmlinuz-linux root=/dev/sdaX rw add_efi_memmap #earlyprintk=xen" >> XEN_CFG_FILE
+echo "extra=\"luks lvm\"" >> XEN_CFG_FILE
+echo "kernel=vmlinuz-linux root=/dev/$VG_LUKS/$LV_ROOT rw add_efi_memmap earlyprintk=xen" >> XEN_CFG_FILE
 echo "ramdisk=initramfs-linux.img" >> XEN_CFG_FILE
+
+# Modificate GRUB
+yay -S grub-xen-git
 
 #FIXME: Adjust UEFI settings
 

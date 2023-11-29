d615ddaec6de13e4499e0b341b87eff53a323c60
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-11-29 17:57:18 +0100

    Create arch_kvm.sh

---
+++ arch_kvm.sh
@@ -0,0 +1,5 @@
+# Update the system
+sudo pacman -Syyu
+
+# Install KVM-related packages
+sudo pacman -S bridge-utils dnsmasq qemu-full virt-manager virt-viewer libguestfs ebtables vde2 openbsd-netcat

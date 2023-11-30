7e02cc8fbb0884b9817c88029e2ef7659b801fde
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-11-30 14:52:16 +0100

    Update arch_desktop_xfce.sh

--- arch_desktop_xfce.sh
+++ arch_desktop_xfce.sh
@@ -13,7 +13,7 @@ sudo pacman --disable-download-timeout --needed --noconfirm -Syu
 sudo pacman --disable-download-timeout --needed --noconfirm -S xorg xorg-drivers
 
 # Install LightDM and Xfce
-sudo pacman --disable-download-timeout --needed --noconfirm -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4
+sudo pacman --disable-download-timeout --needed --noconfirm -S light-locker lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4
 
 # Set dark theme
 xfconf-query -c xsettings -p /Net/ThemeName -s "Adwaita-dark"

ab2bec584195ea9318aee20b33bc54858ce76918
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-11-30 14:55:58 +0100

    Update arch_desktop_xfce.sh

--- arch_desktop_xfce.sh
+++ arch_desktop_xfce.sh
@@ -15,12 +15,15 @@ sudo pacman --disable-download-timeout --needed --noconfirm -S xorg xorg-drivers
 # Install LightDM and Xfce
 sudo pacman --disable-download-timeout --needed --noconfirm -S light-locker lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4
 
+# Install additional applications
+sudo pacman --disable-download-timeout --needed --noconfirm -S mousepad network-manager-applet ristretto thunar-archive-plugin thunar-media-tags-plugin xarchiver xfce4-artwork xfce4-cpugraph-plugin xfce4-mount-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-taskmanager xfce4-whiskermenu-plugin
+
+# Enable screen locking
+xfconf-query -c xfce4-session -p /general/LockCommand -s "light-locker-command -l"
+
 # Set dark theme
 xfconf-query -c xsettings -p /Net/ThemeName -s "Adwaita-dark"
 xfce4-settings-manager --reload
-
-# Install additional applications
-sudo pacman --disable-download-timeout --needed --noconfirm -S mousepad network-manager-applet ristretto thunar-archive-plugin thunar-media-tags-plugin xarchiver xfce4-artwork xfce4-cpugraph-plugin xfce4-mount-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-taskmanager xfce4-whiskermenu-plugin
 
 # Configure LightDM service
 sudo systemctl enable --now lightdm.service

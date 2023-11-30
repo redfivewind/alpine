351b1b392cf261a59e19ed79f5fb2a46b5b09f8b
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-11-30 11:38:10 +0100

    Update arch_desktop_xfce.sh

--- arch_desktop_xfce.sh
+++ arch_desktop_xfce.sh
@@ -20,7 +20,7 @@ xfconf-query -c xsettings -p /Net/ThemeName -s "Adwaita-dark"
 xfce4-settings-manager --reload
 
 # Install additional applications
-sudo pacman --disable-download-timeout --needed --noconfirm -S mousepad ristretto thunar-archive-plugin thunar-media-tags-plugin xarchiver xfce4-artwork xfce4-cpugraph-plugin xfce4-mount-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-taskmanager xfce4-whiskermenu-plugin
+sudo pacman --disable-download-timeout --needed --noconfirm -S mousepad network-manager-applet ristretto thunar-archive-plugin thunar-media-tags-plugin xarchiver xfce4-artwork xfce4-cpugraph-plugin xfce4-mount-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-taskmanager xfce4-whiskermenu-plugin
 
 # Configure LightDM service
 sudo systemctl enable --now lightdm.service

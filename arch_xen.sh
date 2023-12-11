23a35c8680dcdbd0d5d977ca82da64a4ba7e4c02
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-12-11 14:17:01 +0100

    Update arch_xen.sh

--- arch_xen.sh
+++ arch_xen.sh
@@ -1,13 +1,19 @@
 #FIXME: REVIEW ANYTHING!
 
+# Notes
+# Reference: https://wiki.archlinux.org/title/xen
+
+# Global variables
+LV_ROOT="root" # Label & name of the root partition
+VG_LUKS="vg_luks" # LUKS volume group
+XEN_CFG_FILE="/boot/efi/xen.cfg" # Xen EFI boot configuration file
+
 # Install Xen packages
 yay -S xen xen-qemu
-#sudo pacman -S edk2-ovmf # Fur UEFI support in virtual machines
-#sudo pacman -S seabios # For BIOS support in virtual machines
+sudo pacman -S edk2-ovmf # For UEFI support in virtual machines
+sudo pacman -S seabios # For BIOS support in virtual machines
 
 # Modificate the bootloader
-XEN_CFG_FILE="/boot/efi/xen.cfg"
-
 echo "[global]" > XEN_CFG_FILE
 echo "default=xen" >> XEN_CFG_FILE
 echo >> XEN_CFG_FILE
@@ -18,7 +24,9 @@ echo "kernel=vmlinuz-linux root=/dev/$VG_LUKS/$LV_ROOT rw add_efi_memmap earlypr
 echo "ramdisk=initramfs-linux.img" >> XEN_CFG_FILE
 
 # Modificate GRUB
-yay -S grub-xen-git
+yay --disable-download-timeout --needed --noconfirm -S grub-xen-git
+sudo echo "GRUB_CMDLINE_XEN_DEFAULT=\"dom0_mem=512M\"" >> /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 #FIXME: Adjust UEFI settings
 

be58cd7812b03204a08e2468d8f11341a4de921a
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-12-13 14:34:46 +0100

    Update arch_yay.sh

--- arch_yay.sh
+++ arch_yay.sh
@@ -1,1 +1,22 @@
+# Retrieve yay
+git clone https://aur.archlinux.org/yay-bin.git /home/$USER_NAME/tools/yay-bin
+cd /home/$USER_NAME/tools/yay-bin
 
+# Install yay
+makepkg --noconfirm -si
+cd
+yay --version
+
+# Install required tools
+TOOLS="chkrootkit onboard secure-delete"
+
+for Tool in $TOOLS; do
+    sudo pacman --disable-download-timeout --needed --noconfirm -S $Tool
+    yay --disable-download-timeout --needed --noconfirm -S $Tool
+done
+
+# Cleanup
+srm -v -r /home/$USER_NAME/tools/arch_yay.sh
+srm -v -r /home/$USER_NAME/tools/yay-bin
+
+sudo pacman --noconfirm -Rns $(pacman -Qdtq)

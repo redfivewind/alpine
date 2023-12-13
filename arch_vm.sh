40a2d33ea3ede9d1a20fc9625b69cdd9174345bb
alexschweer <36482775+alexschweer@users.noreply.github.com>
2023-12-13 14:31:20 +0100

    Delete arch_vm.sh

--- arch_vm.sh
+++
@@ -1,8 +0,0 @@
-#FIXME: Everything
-
-# Install the VirtualBox guest additions
-sudo pacman -Sy --disable-download-timeout --needed --noconfirm virtualbox-guest-utils
-
-# Enable & start the VirtualBox service
-sudo systemctl enable vboxservice.service
-sudo systemctl start vboxservice.service

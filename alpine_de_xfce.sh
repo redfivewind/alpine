4ce9c917303149a1776946c0db84603c56ba56f4
alexschweer <36482775+alexschweer@users.noreply.github.com>
2024-01-12 15:11:29 +0100

    Update alpine_de_xfce.sh

--- alpine_de_xfce.sh
+++ alpine_de_xfce.sh
@@ -1,20 +1,22 @@
+#FIXME: xfconf-query -c xfce4-session -p /general/LockCommand -s "light-locker-command -l"
+
 # German keyboard layout
 echo "[*] Loading German keyboard layout..."
 doas setup-keymap de de
 
 # Xfce basic installation
 echo "[*] Installing Xfce..."
-doas setup-desktop xfce
-sleep 2
-
-# Further required packages
-echo "[*] Installing further required packages..."
-doas apk add adw-gtk3 \
-    elogind \
+doas setup-xorg-base adw-gtk3 \
+    consolekit2 \
+    dbus \
+	dbus-x11 \
+	font-dejavu \
     gvfs \
+    lightdm \
+	lightdm-gtk-greeter \
     mousepad \
     pavucontrol \
-    polkit-elogind \
+    polkit \
     ristretto \
     thunar-archive-plugin \
     xarchiver \ 
@@ -23,16 +25,9 @@ doas apk add adw-gtk3 \
     xfce4-pulseaudio-plugin \
     xfce4-screenshooter \
     xfce4-taskmanager \
+    xfce4-terminal \
     xfce4-whiskermenu-plugin
 #thunar-media-tags-plugin
-
-# Configure services
-echo "[*] Configuring services..."
-doas rc-update add elogind default
-
-# Start services
-echo "[*] Starting services..."
-doas rc-service elogind start
 
 # Xfce keyboard layout
 echo "[*] Setting the Xfce keyboard layout to German..."
@@ -41,4 +36,15 @@ xfconf-query -c keyboard-layout -p /Default/XkbLayout -s de
 # Dark mode
 echo "[*] Enabling dark mode..."
 xfconf-query -c xsettings -p /Net/ThemeName -s "Adwaita-dark"
+
+# Configure services
+echo "[*] Configuring services..."
+doas rc-update add dbus default
+doas rc-update add lightdm default
+
+# Start services
+echo "[*] Starting services..."
+doas rc-service dbus start
+doas rc-service lightdm start
+
 

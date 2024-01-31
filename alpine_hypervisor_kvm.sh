58929e11844e88055878fb67cce879352d51ffa3
alexschweer <36482775+alexschweer@users.noreply.github.com>
2024-01-31 14:36:10 +0100

    Update alpine_hypervisor_kvm.sh

--- alpine_hypervisor_kvm.sh
+++ alpine_hypervisor_kvm.sh
@@ -1,1 +1,27 @@
+# TODO: Everything!
 
+# Install required packages
+echo "[*] Installing required packages..."
+apk add bridge \
+    bridge-utils \
+    dmidecode \
+    ebtables \
+    libvirt \
+    libvirt-daemon \
+    netcat-openbsd \
+    ovmf \
+    seabios \
+    virt-manager \
+    virt-viewer
+sleep 2
+
+# Configure services
+echo "[*] Configuring required services..."
+chroot /mnt rc-update add libvirt-guests default
+chroot /mnt rc-update add libvirtd default
+
+echo "[*] Adding the user to the 'libvirt' group..."
+chroot /mnt adduser $USER_NAME libvirt
+
+# KVM nested virtualisation
+#FIXME

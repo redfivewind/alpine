f4884bb6e9dd94fdde16ac7a9e63ca67618756fc
alexschweer <36482775+alexschweer@users.noreply.github.com>
2024-01-31 14:11:13 +0100

    Update alpine_hypervisor_xen.sh

--- alpine_hypervisor_xen.sh
+++ alpine_hypervisor_xen.sh
@@ -1,1 +1,32 @@
+# Install required packages
+echo "[*] Installing required packages..."
+apk add bridge \
+    bridge-utils \
+    dmidecode \
+    ebtables \
+    libvirt \
+    libvirt-daemon \
+    netcat-openbsd \
+    ovmf \
+    seabios \
+    virt-manager \
+    virt-viewer \
+    xen-hypervisor \
+    xen-qemu
+sleep 2
 
+# Configure Alpine Linux as Xen dom0
+echo "[*] Configuring Alpine Linux as Xen dom0..."
+setup-xen-dom0
+sleep 2
+
+# Configure services
+echo "[*] Configuring required services..."
+chroot /mnt rc-update add libvirt-guests default
+chroot /mnt rc-update add libvirtd default
+
+echo "[*] Adding the user to the 'libvirt' group..."
+chroot /mnt adduser $USER_NAME libvirt
+
+# Generate Xen unified kernel image (UKI)
+#FIXME

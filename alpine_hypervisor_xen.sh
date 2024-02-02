35fff42d65edb8874c63a6136fc89a6a642b1eeb
alexschweer <36482775+alexschweer@users.noreply.github.com>
2024-02-02 15:51:37 +0100

    Update alpine_hypervisor_xen.sh

--- alpine_hypervisor_xen.sh
+++ alpine_hypervisor_xen.sh
@@ -13,19 +13,23 @@ doas apk add bridge \
     virt-viewer \
     xen \
     xen-hypervisor \
-    xen-libs \
     xen-qemu
 sleep 2
 
-# Configure Alpine Linux as Xen dom0
-echo "[*] Configuring Alpine Linux as Xen dom0..."
-doas setup-xen-dom0
-sleep 2
+# Enable modules
+echo "[*] Enabling modules..."
+echo "xen-blkback" | doas tee -a /etc/modules
+echo "xen-netback" | doas tee -a /etc/modules
+echo "tun" | doas tee -a /etc/modules
 
 # Configure services
 echo "[*] Configuring required services..."
 doas rc-update add libvirt-guests default
-doas /mnt rc-update add libvirtd default
+doas rc-update add libvirtd default
+doas rc-update add xenconsoled default
+doas rc-update add xendomains default 
+doas rc-update add xenqemu default
+doas rc-update add xenstored default
 sleep 2
 
 # Add user to the 'libvirt' group

40524ee427a18c421f9e99a213c772972cd99aaf
alexschweer <36482775+alexschweer@users.noreply.github.com>
2024-02-02 14:54:48 +0100

    Update alpine_hypervisor_xen.sh

--- alpine_hypervisor_xen.sh
+++ alpine_hypervisor_xen.sh
@@ -1,6 +1,6 @@
 # Install required packages
 echo "[*] Installing required packages..."
-apk add bridge \
+doas apk add bridge \
     bridge-utils \
     dmidecode \
     ebtables \
@@ -18,19 +18,17 @@ sleep 2
 
 # Configure Alpine Linux as Xen dom0
 echo "[*] Configuring Alpine Linux as Xen dom0..."
-setup-xen-dom0
+doas setup-xen-dom0
 sleep 2
 
 # Configure services
 echo "[*] Configuring required services..."
-chroot /mnt rc-update add libvirt-guests default
-chroot /mnt rc-update add libvirtd default
+doas rc-update add libvirt-guests default
+doas /mnt rc-update add libvirtd default
 
+# Add user to the 'libvirt' group
 echo "[*] Adding the user to the 'libvirt' group..."
-chroot /mnt adduser $USER_NAME libvirt
-
-#echo "GRUB_CMDLINE_XEN=\"console=vga guest_loglvl=all loglvl=all nomodeset noreboot=true\"" >> /mnt/etc/default/grub
-#echo "GRUB_CMDLINE_XEN_DEFAULT=\"ucode=scan\"" >> /mnt/etc/default/grub #dom0_max_vcpus=1 dom0_vcpus_pin maxmem=512
+doas adduser $USER_NAME libvirt
 
 # Generate Xen unified kernel image (UKI)
 #FIXME

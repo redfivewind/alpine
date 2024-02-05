9791e492cbb61d6706b49b0c5c3c31e246597650
alexschweer <36482775+alexschweer@users.noreply.github.com>
2024-02-05 12:15:48 +0100

    Update win11_core.ps1

--- win11_core.ps1
+++ win11_core.ps1
@@ -1,6 +1,10 @@
+# Administrator check
+
 # Windows Explorer
 
 # Dark mode
+New-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Name 'SystemUsesLightTheme' -Value '0' -PropertyType DWORD -Force
+New-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Name 'AppsUseLightTheme' -Value '0' -PropertyType DWORD -Force
 
 # Chocolate software
 

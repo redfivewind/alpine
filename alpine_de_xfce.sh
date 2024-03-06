32ee74e4447d84b11764dcf589e51767b6a8dc34
alexschweer <36482775+alexschweer@users.noreply.github.com>
2024-03-06 12:09:59 +0100

    Update alpine_de_xfce.sh

--- alpine_de_xfce.sh
+++ alpine_de_xfce.sh
@@ -43,6 +43,7 @@ echo "EndSection" | doas tee -a /etc/X11/xorg.conf.d/00-keyboard.conf
 echo "[*] Customising Xfce..."
 export $(dbus-launch)
 xfconf-query -c xsettings -p /Net/ThemeName -s "adw-gtk3-dark"
+wget --output-document=/home/user/Pictures/alpine.png https://raw.githubusercontent.com/alexschweer/infra/main/alpine.png
 xfconf-query -c xfce4-desktop -p  /backdrop/screen0/monitor0/workspace0/last-image -s /home/user/Pictures/alpine.png
 xfconf-query -c xfce4-keyboard-shortcuts -n -t 'string' -p '/commands/custom/<Super>L' -s xflock4
 

af47ea34cd7fe4c1d01e6ee6383e221bf70d42fa
alexschweer <36482775+alexschweer@users.noreply.github.com>
2024-03-13 13:27:14 +0100

    Update alpine_uefi_sb.sh

--- alpine_uefi_sb.sh
+++ alpine_uefi_sb.sh
@@ -5,20 +5,23 @@ sleep 3
 
 # Install required packages
 echo "[*] Installing required packages..."
-doas apk add efibootmgr gummiboot-efistub secureboot-hook
+doas apk add sbctl
 sleep 3
 
-# Adjust the 'cmdline' in '/etc/kernel-hooks.d/secureboot.conf'
-echo "[*] Adjusting the 'cmdline' in '/etc/kernel-hooks.d/secureboot.conf'..."
-
+# Make the 'cmdline" available in '/etc/kernel/cmdline'
+echo "[*] Making the 'cmdline' available in '/etc/kernel/cmdline'..."
+doas cat /proc/cmdline | doas tee /etc/kernel/cmdline
 sleep 3
 
-# Run the kernel hooks
-echo "[*] Running the kernel hooks..."
-doas apk fix kernel-hooks
-sleep 3
+# Generate & enroll UEFI keys using sbctl
+echo "[*] Generating signing keys using sbctl..."
+doas sbctl create-keys
 
-# Disable the mkinitfs trigger
-echo "[*] Disabling the mkinitfs trigger..."
-echo 'disable_trigger=yes' >> /etc/mkinitfs/mkinitfs.conf
-sleep 3
+echo "[*] Enrolling the signing keys using sbctl..."
+doas sbctl enroll-keys
+
+# Protect Alpine Linux with Secure Boot
+#FIXME
+
+# Protect Xen with Secure Boot (if applicable)
+#FIXME

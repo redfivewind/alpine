9952fcb7c3469c314d2caba34b7256fdf8e2a85f
alexschweer <36482775+alexschweer@users.noreply.github.com>
2024-03-13 13:41:40 +0100

    Update alpine_uefi_sb.sh

--- alpine_uefi_sb.sh
+++ alpine_uefi_sb.sh
@@ -13,15 +13,27 @@ echo "[*] Making the 'cmdline' available in '/etc/kernel/cmdline'..."
 doas cat /proc/cmdline | doas tee /etc/kernel/cmdline
 sleep 3
 
+# Initially show 'sbctl status'
+echo "[*] Retrieving the UEFI Secure Boot status..."
+doas sbctl status
+sleep 3
+
 # Generate & enroll UEFI keys using sbctl
 echo "[*] Generating signing keys using sbctl..."
 doas sbctl create-keys
+sleep 3
 
 echo "[*] Enrolling the signing keys using sbctl..."
 doas sbctl enroll-keys
+sleep 3
 
 # Protect Alpine Linux with Secure Boot
 #FIXME
 
 # Protect Xen with Secure Boot (if applicable)
 #FIXME
+
+# Finally, show 'sbctl status again'
+echo "[*] Finally, verifying the UEFI Secure Boot status again..."
+doas sbctl status
+sleep 3

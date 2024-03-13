8718828d616db5a1252f6399719fd3d2976b2bc2
alexschweer <36482775+alexschweer@users.noreply.github.com>
2024-03-13 12:01:11 +0100

    Update alpine_uefi_sb.sh

--- alpine_uefi_sb.sh
+++ alpine_uefi_sb.sh
@@ -1,1 +1,24 @@
+# Warning message
+echo "[!] ALERT: Make sure that the UEFI is in setup mode. Press any key to continue..."
+read
+sleep 3
 
+# Install required packages
+echo "[*] Installing required packages..."
+doas apk add efibootmgr gummiboot-efistub secureboot-hook
+sleep 3
+
+# Adjust the 'cmdline' in '/etc/kernel-hooks.d/secureboot.conf'
+echo "[*] Adjusting the 'cmdline' in '/etc/kernel-hooks.d/secureboot.conf'..."
+
+sleep 3
+
+# Run the kernel hooks
+echo "[*] Running the kernel hooks..."
+doas apk fix kernel-hooks
+sleep 3
+
+# Disable the mkinitfs trigger
+echo "[*] Disabling the mkinitfs trigger..."
+echo 'disable_trigger=yes' >> /etc/mkinitfs/mkinitfs.conf
+sleep 3

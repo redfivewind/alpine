3f4b93f7c42652e6dc7c8f5024d5f628ab271335
alexschweer <36482775+alexschweer@users.noreply.github.com>
2024-03-19 16:12:42 +0100

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -2,10 +2,11 @@
 # ARGUMENT: Disk
 # ARGUMENT: Mode (core, core_kvm, core_xen, core_virt)
 # ARGUMENT: Desktop Environment (none, xfce)
+# ARGUMENT: ---Audio---
+# ARGUMENT: ---Bluetooth---
+# ARGUMENT: ---Network (Broadband, Ethernet, General, WiFi, ...)---
 
-# ARGUMENT: Audio
-# ARGUMENT: Bluetooth
-# ARGUMENT: Network (Broadband, Ethernet, General, WiFi, ...)
+# NOTES: Always with community repository?
 
 # TODO: Core: Automatic sleep/hibernate & resume
 # TODO: Core: HARDENING!!!
@@ -31,13 +32,173 @@ USER_NAME="user"
 USER_PASS=""
 
 # Argument parsing
-
+#FIXME
 
 # German keyboard layout
 echo "[*] Loading German keyboard layout..."
 setup-keymap de de
 
+# Retrieve the LUKS & user password
+echo "[*] Please enter the LUKS password: "
+read -s luks_pass_a
+echo "[*] Please reenter the LUKS password: "
+read -s luks_pass_b
+
+if [ "$luks_pass_a" == "$luks_pass_b" ]; then
+    LUKS_PASS=$luks_pass_a
+else
+    echo "[X] ERROR: The LUKS passwords do not match."
+    exit 1
+fi
+
+echo "[*] Please enter the user password: "
+read -s user_pass_a
+echo "[*] Please reenter the user password: "
+read -s user_pass_b
+
+if [ "$user_pass_a" == "$user_pass_b" ]; then
+    USER_PASS=$user_pass_a
+else
+    echo "[X] ERROR: The user passwords do not match."
+    exit 1
+fi
+
+# User management
+echo "[*] Setting up a standard user..."
+setup-user -a -f "$USER_NAME" $USER_NAME
+sleep 2
+
+# Set the hostname
+echo "[*] Setting the hostname..."
+setup-hostname workstation
+
+# Set the timezone
+echo "[*] Setting the timezone..."
+setup-timezone Europe/Berlin
+
+# Network time synchronisation
+echo "[*] Enabling network time synchronization..."
+setup-ntp busybox
+
+# Disable SSHD
+echo "[*] Disabling SSHD..."
+setup-sshd none
+
+# Configure apk
+echo "[*] Configuring apk & enabling the Alpine community repository..."
+setup-apkrepos -c -f
+
+# Install required packages
+echo "[*] Installing required packages..."
+apk add amd-ucode \
+    cryptsetup \
+    e2fsprogs \
+    file \    
+    lsblk \
+    lvm2 \
+    nano \
+    p7zip \
+    sgdisk \
+    tlp \
+    unzip \
+    zip
+sleep 2
+
+# Setup udev as devd
+echo "[*] Setting up udev as devd..."
+setup-devd udev
+
+# Partitioning
 #FIXME
+
+# Setup LUKS partition
+echo "[*] Formatting the second partition as a LUKS crypto partition..."
+echo -n $LUKS_PASS | cryptsetup luksFormat $PART_LUKS --type luks1 -c twofish-xts-plain64 -h sha512 -s 512 --iter-time 10000 -
+echo -n $LUKS_PASS | cryptsetup luksOpen $PART_LUKS $LUKS_LVM -
+sleep 2
+
+# Setup LVM within LUKS partition
+echo "[*] Setting up LVM..."
+pvcreate /dev/mapper/$LUKS_LVM
+vgcreate $LVM_VG /dev/mapper/$LUKS_LVM
+lvcreate -L 6144M $LVM_VG -n $LV_SWAP
+lvcreate -l 100%FREE $LVM_VG -n $LV_ROOT
+sleep 2
+
+# Format logical volumes
+echo "[*] Formatting the partitions..."
+mkfs.vfat $PART_EFI
+mkfs.ext4 /dev/mapper/$LVM_VG-$LV_ROOT
+mkswap /dev/mapper/$LVM_VG-$LV_SWAP -L $LV_SWAP
+swapon /dev/$LVM_VG/$LV_SWAP
+sleep 2
+
+# Mount #FIXME
+echo "#FIXME"
+#FIXME
+sleep 2
+
+# Install Alpine
+echo "[*] Installing Alpine Linux..."
+setup-disk -m sys /mnt/
+
+# Set mkinitfs settings & modules
+echo "[*] Adding LVM and crypto modules to mkinitfs..."
+echo "features=\"ata base ide scsi usb virtio ext4 lvm nvme keymap cryptsetup cryptkey resume\"" | tee /mnt/etc/mkinitfs/mkinitfs.conf
+mkinitfs -c /mnt/etc/mkinitfs/mkinitfs.conf -b /mnt/ $(ls /mnt/lib/modules/)
+
+# Mount required filesystems
+echo "[*] Mounting required filesystems..."
+mount -t proc /proc /mnt/proc
+mount --rbind /dev /mnt/dev
+mount --make-rslave /mnt/dev
+mount --rbind /sys /mnt/sys
+
+# Set 'noatime' within the fstab
+echo "[*] Setting 'noatime' within the fstab file..."
+sed -i 's/relatime/noatime/g' /mnt/etc/fstab
+cat /mnt/etc/fstab
+sleep 2
+
+# Setup GRUB
+echo "[*] Configuring GRUB for encrypted boot..."
+chroot /mnt apk add efibootmgr grub grub-efi
+echo "GRUB_ENABLE_CRYPTODISK=y" >> /mnt/etc/default/grub
+echo "GRUB_PRELOAD_MODULES=\"cryptodisk luks lvm part_gpt\"" >> /mnt/etc/default/grub
+sed -i 's/GRUB_TIMEOUT=2/GRUB_TIMEOUT=10/' /mnt/etc/default/grub
+tail /mnt/etc/default/grub
+sleep 2
+
+echo "[*] Installing GRUB..."
+#FIXME: chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi
+chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+chroot /mnt chmod 700 /boot
+sleep 2
+
+# Configure required services
+echo "[*] Configuring required services..."
+chroot /mnt rc-update add tlp default
+sleep 2  
+
+# User security
+echo "[*] Disabling the root account..."
+chroot /mnt passwd -l root
+
+echo "[*] Setting the user password..."
+echo -n "$USER_NAME:$USER_PASS" | chpasswd -R /mnt
+
+echo "[*] Adding the user to required groups..."
+chroot /mnt addgroup -S netdev
+chroot /mnt adduser $USER_NAME netdev
+chroot /mnt addgroup -S plugdev
+chroot /mnt adduser $USER_NAME plugdev
+
+# Add user paths & scripts
+echo "[*] Adding user paths & scripts..."
+mkdir -p /mnt/home/$USER_NAME/pictures
+mkdir -p /mnt/home/$USER_NAME/tools
+mkdir -p /mnt/home/$USER_NAME/workspace
+chroot /mnt chown -R $USER_NAME:users /home/$USER_NAME/   
 
 # Stop message
 echo "[*] Work done. Returning..."

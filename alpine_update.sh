8a34729553665954428eb045e421e83454a9238c
alexschweer <36482775+alexschweer@users.noreply.github.com>
2024-03-19 08:33:40 +0100

    Update alpine_update.sh

--- alpine_update.sh
+++ alpine_update.sh
@@ -2,10 +2,5 @@
 echo "[*] Updating packages via apk..."
 doas apk upgrade
 
-# Secure boot patch for Alpine Linux
-echo "[*] Applying the UEFI Secure Boot patch to Alpine Linux if required..."
-#FIXME
-
-# Secure boot patch for Xen
-echo "[*] Applying the UEFI Secure Boot patch to Xen if required..."
+#FIXME: Whatever
 #FIXME

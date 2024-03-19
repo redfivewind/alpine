8b6d596ddc2d01990d6fa7979083bd7cb8708497
alexschweer <36482775+alexschweer@users.noreply.github.com>
2024-03-19 15:45:01 +0100

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -1,6 +1,5 @@
 # ARGUMENT: Platform (bios, uefi, uefi-sb)
 # ARGUMENT: Mode (none, hv_kvm, hv_xen, virt)
-# ARGUMENT: Enable LUKS?
 # ARGUMENT: Switch (audio, ethernet, network, wireless)
 # ARGUMENT: Hardening
 # ARGUMENT: Desktop Environment (none, xfce)
@@ -10,7 +9,7 @@ echo "[!] ALERT: This script is potentially destructive. Use it on your own risk
 
 # Global variables
 echo "[*] Initializing global variables..."
-DEV=""
+DISK=""
 LUKS_PASS=""
 LV_ROOT="lv_root"
 LV_SWAP="lv_swap"
@@ -18,8 +17,20 @@ LVM_VG="lvm_vg"
 LUKS_LVM="luks_lvm"
 PART_EFI=""
 PART_LUKS=""
+PLATFORM=""
 SCRIPT=$(readlink -f "$0")
 USER_NAME="user"
 USER_PASS=""
 
 # Argument parsing
+
+
+# German keyboard layout
+echo "[*] Loading German keyboard layout..."
+setup-keymap de de
+
+#FIXME
+
+# Stop message
+echo "[*] Work done. Returning..."
+return

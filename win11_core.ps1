0d2e18cc407f97c2b9adca5bfcfe6ff511a6fd18
alexschweer <36482775+alexschweer@users.noreply.github.com>
2024-03-24 21:29:20 +0100

    Update win11_core.ps1

--- win11_core.ps1
+++ win11_core.ps1
@@ -2,17 +2,20 @@
 Write-Output "[*] Configuring Windows 11..."
 
 # Administrator check
+Write-Output "[*] Checking whether the current user has adminstrator rights..."
 $l_current_user = [Security.Principal.WindowsIdentity]::GetCurrent()
 $l_windows_principal = New-Object Security.Principal.WindowsPrincipal($l_current_user)
 
 if ($l_windows_principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Output "[*] The current user has Administrator rights..."
+    Write-Output "[*] The current user has administrator rights..."
 } else {
     Write-Output "[X] ERROR: The current user does not have Administrator rights. Returning..."
     return
 }
 
 # Windows Explorer
+Write-Output "[*] Configuring Windows Explorer..."
+#FIXME
 
 # Dark mode
 Write-Output "[*] Enabling dark mode..."
@@ -24,10 +27,13 @@ Write-Output "[*] Installing Chocolate software management..."
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Base packages
-choco install 
+Write-Output "[*] Installing base packages..."
+choco install 7zip ccleaner librewolf vlc vscodium --acceptlicence --confirm --ignoredetectedreboot
 
 # Windows Updates
+Write-Output "[*] Installing Windows updates..."
+#FIXME: Special updates
 Install-Module -Name PSWindowsUpdate -Force
 Update-WUModule
 Get-WUList -MicrosoftUpdate
-Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
+Install-WindowsUpdate -AcceptAll -IgnoreReboot -MicrosoftUpdate

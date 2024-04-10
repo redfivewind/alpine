6294a62a51b3477f1924f57f4ca893ad80319145
redfivewind <36482775+redfivewind@users.noreply.github.com>
2024-04-10 14:56:44 +0200

    Update alpine_hv_kvm.sh

--- alpine_hv_kvm.sh
+++ alpine_hv_kvm.sh
@@ -1,3 +1,8 @@
+# Start message
+echo "[*] This script installs the KVM virtualisation infrastructure on this Alpine Linux system."
+echo "[!] ALERT: This script is potentially destructive. Use it on your own risk. Press any key to continue..."
+read
+
 # Install required packages
 echo "[*] Installing required packages..."
 doas apk add bridge \
@@ -18,13 +23,35 @@ sleep 2
 echo "[*] Enabling modules..."
 echo "tun" | doas tee -a /etc/modules
 
+# Add user to the 'libvirt' group
+echo "[*] Adding user '$USER_NAME' to required groups.."
+doas adduser $(whoami) kvm
+doas adduser $(whoami) libvirt
+doas adduser $(whoami) qemu
+sleep 2
+
 # Configure services
 echo "[*] Configuring required services..."
 doas rc-update add libvirt-guests default
 doas rc-update add libvirtd default
 sleep 2
 
-# Add user to the 'libvirt' group
-echo "[*] Adding the user to the 'libvirt' group..."
-doas adduser $(whoami) libvirt
-sleep 2
+# Cleanup
+echo "[*] Should this script be deleted? (yes/no)"
+read delete_script
+
+if [ "$delete_script" == "yes" ];
+then
+    echo "[*] Deleting the script..."
+    shred --force --remove=wipesync --verbose --zero $(readlink -f $0)
+elif [ "$delete_script" == "no" ];
+then
+    echo "[*] Skipping script deletion..."
+else
+    echo "[!] ALERT: Variable 'delete_script' is '$delete_script' but must be 'yes' or 'no'."
+fi
+
+# Stop message
+echo "[*] Installation of the KVM virtualisation infrastructure is finished. You may reboot now."
+echo "[*] Work done. Exiting..."
+exit 0

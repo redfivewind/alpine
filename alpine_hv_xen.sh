f50e19fff09dbe7456dd7d8dee4409a77eb1a93a
redfivewind <36482775+redfivewind@users.noreply.github.com>
2024-04-15 18:16:28 +0200

    Update alpine_hv_xen.sh

--- alpine_hv_xen.sh
+++ alpine_hv_xen.sh
@@ -65,7 +65,7 @@ echo '[global]' | doas tee $TMP_XEN_CFG
 echo 'default=alpine-linux' | doas tee -a $TMP_XEN_CFG
 echo '' | doas tee -a $TMP_XEN_CFG
 echo "[alpine-linux]" | doas tee -a $TMP_XEN_CFG
-echo "options=console=vga flask=disabled iommu=verbose loglvl=all ucode=scan" | doas tee -a $TMP_XEN_CFG
+echo "options=console=vga flask=disabled iommu=dom0-passthrough loglvl=all" | doas tee -a $TMP_XEN_CFG
 echo "kernel=vmlinuz-lts $(cat /etc/kernel/cmdline)" | doas tee -a $TMP_XEN_CFG
 echo "ramdisk=initramfs-lts" | doas tee -a $TMP_XEN_CFG
 sleep 3

cbdc3061ee8043a77d90e36c6eec2819cf81045c
redfivewind <36482775+redfivewind@users.noreply.github.com>
2025-09-11 12:47:53 +0000

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -520,7 +520,7 @@ _03_06_setup_boot_env() {
         chroot /mnt sbctl create-keys
 
         echo '[*] Enrolling the signing keys for UEFI Secure Boot...'
-        chroot /mnt sbctl enroll-keys --ignore-immutable --microsoft
+        chroot /mnt doas sbctl enroll-keys --ignore-immutable --microsoft
         
         echo '[*] Generating a unified kernel image for Alpine Linux...'
         chroot /mnt sbctl bundle --amducode /boot/amd-ucode.img \

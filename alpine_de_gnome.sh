76c1a17d374345c3ccfc47f8c7bac4fb1443e512
redfivewind <36482775+redfivewind@users.noreply.github.com>
2025-09-26 11:30:54 +0000

    Update alpine_de_gnome.sh

--- alpine_de_gnome.sh
+++ alpine_de_gnome.sh
@@ -5,43 +5,43 @@ read
 
 # System update
 echo "[*] Updating the system..."
-doas apk update
-doas apk upgrade
+sudo apk update
+sudo apk upgrade
 
 # Install Wayland
 echo "[*] Installing Wayland..."
-doas setup-wayland-base
+sudo setup-wayland-base
 
 # Install Hyprland
 echo "[*] Installing required GNOME packages..."
-doas apk add elogind gdm gnome-control-center gnome-session gnome-settings-daemon gnome-shell gnome-terminal gnome-tweaks libva mesa mutter spice-vdagent
+sudo apk add elogind gdm gnome-control-center gnome-session gnome-settings-daemon gnome-shell gnome-terminal gnome-tweaks libva mesa mutter spice-vdagent
 
 # Install required packages
 echo "[*] Installing further packages..."
-doas apk add baobab evince gnome-calculator gnome-clocks gnome-disk-utility gnome-screenshot gnome-system-monitor gnome-text-editor loupe
+sudo apk add baobab evince gnome-calculator gnome-clocks gnome-disk-utility gnome-screenshot gnome-system-monitor gnome-text-editor loupe
 
 # Configure networking
 echo "[*] Configuring networking..."
 
 echo "[*] Removing package 'wpa_supplicant'..."
-doas apk del wpa_supplicant
+sudo apk del wpa_supplicant
 
 echo "[*] Configuring package 'NetworkManager'..."
-echo "[main]" | doas tee /etc/NetworkManager/NetworkManager.conf
-echo "dhcp=internal" | doas tee -a /etc/NetworkManager/NetworkManager.conf
-echo "plugins=ifupdown,keyfile" | doas tee -a /etc/NetworkManager/NetworkManager.conf
-echo "" | doas tee -a /etc/NetworkManager/NetworkManager.conf
-echo "[ifupdown]" | doas tee -a /etc/NetworkManager/NetworkManager.conf
-echo "managed=true" | doas tee -a /etc/NetworkManager/NetworkManager.conf
-echo "" | doas tee -a /etc/NetworkManager/NetworkManager.conf
-echo "[device]" | doas tee -a /etc/NetworkManager/NetworkManager.conf
-echo "wifi.scan-rand-mac-address=yes" | doas tee -a /etc/NetworkManager/NetworkManager.conf
-echo "wifi.backend=iwd" | doas tee -a /etc/NetworkManager/NetworkManager.conf
+echo "[main]" | sudo tee /etc/NetworkManager/NetworkManager.conf
+echo "dhcp=internal" | sudo tee -a /etc/NetworkManager/NetworkManager.conf
+echo "plugins=ifupdown,keyfile" | sudo tee -a /etc/NetworkManager/NetworkManager.conf
+echo "" | sudo tee -a /etc/NetworkManager/NetworkManager.conf
+echo "[ifupdown]" | sudo tee -a /etc/NetworkManager/NetworkManager.conf
+echo "managed=true" | sudo tee -a /etc/NetworkManager/NetworkManager.conf
+echo "" | sudo tee -a /etc/NetworkManager/NetworkManager.conf
+echo "[device]" | sudo tee -a /etc/NetworkManager/NetworkManager.conf
+echo "wifi.scan-rand-mac-address=yes" | sudo tee -a /etc/NetworkManager/NetworkManager.conf
+echo "wifi.backend=iwd" | sudo tee -a /etc/NetworkManager/NetworkManager.conf
 
 # Configure services
 echo "[*] Configuring services..."
-doas rc-update add elogind
-doas rc-update add gdm
+sudo rc-update add elogind
+sudo rc-update add gdm
 
 # Clean up
 echo "[*] Cleaning up..."
